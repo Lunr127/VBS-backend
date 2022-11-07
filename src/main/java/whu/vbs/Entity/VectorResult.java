241cccf8779f4219080bfca7f867e57752559b9d
Lunr_Lee <Lunr_Lee@outlook.com>
2022-11-07 20:58:47 +0800

    Vector Entity

---
+++ src/main/java/whu/vbs/Entity/VectorResult.java
@@ -0,0 +1,10 @@
+package whu.vbs.Entity;
+
+import lombok.Data;
+
+@Data
+public class VectorResult {
+    private Integer id;
+    private String path;
+    private String vector;
+}
---
+++ src/main/java/whu/vbs/Mapper/VectorMapper.java
@@ -0,0 +1,9 @@
+package whu.vbs.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+import whu.vbs.Entity.VectorResult;
+
+@Repository
+public interface VectorMapper extends BaseMapper<VectorResult> {
+}

35d91f9ae14b67e7505995d08d1d071e7b66866f
Lunr_Lee <Lunr_Lee@outlook.com>
2022-11-21 08:50:32 +0800

    add GrandTruth

---
+++ src/main/java/whu/vbs/Entity/GrandTruth.java
@@ -0,0 +1,13 @@
+package whu.vbs.Entity;
+
+import lombok.Data;
+
+@Data
+public class GrandTruth {
+    private Integer id;
+    private Integer query;
+    private Integer junk;
+    private String shot;
+    private Integer stratum;
+    private Integer judgment;
+}

73ee6b793faf006cee9986bdda597c9e155bf93c
Lunr_Lee <Lunr_Lee@outlook.com>
2022-12-02 16:48:14 +0800

    labelTest

--- src/test/java/whu/vbs/grandTruthTests.java
+++ src/test/java/whu/vbs/grandTruthTests.java
@@ -174,7 +174,7 @@ public class grandTruthTests {
         //调用 python 函数得到新的查询向量
         try {
             //执行 py 文件
-            String[] args1 = new String[]{"E:\\Git\\lavis2\\venv\\Scripts\\python.exe", "E:\\Git\\lavis2\\qmr.py", String.valueOf(vectorCosineSimilarity), vectorList};
+            String[] args1 = new String[]{"E:\\Git\\lavis2\\venv\\Scripts\\python.exe", "E:\\Git\\lavis2\\qir.py", String.valueOf(vectorCosineSimilarity), vectorList};
             Process proc = Runtime.getRuntime().exec(args1);
             BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 
@@ -302,5 +302,179 @@ public class grandTruthTests {
         }
     }
 
+    @Test
+    void labelTest(){
+        Map<String, Double> scoreMap = new HashMap<>();//（路径，得分）键值对
+        List<String> urlList = new ArrayList<>();//查询结果的路径
+        Map<String, List<Double>> pathMap = new HashMap<>();//（路径，向量）键值对
 
+
+        int topK = 200;
+        int total = 0;
+
+
+        int query;
+
+        for (query = 1661; query <= 1680; query++) {
+            String queryNumber = Integer.toString(query).substring(1);
+            CsvReader reader = CsvUtil.getReader();
+            String csvPath = "D:\\Download\\VBSDataset\\grand_truth\\" + queryNumber + ".csv";
+            List<GrandTruthResult> result = reader.read(ResourceUtil.getUtf8Reader(csvPath), GrandTruthResult.class);
+
+            List<String> pathList = new ArrayList<>();
+
+            Query queryVector = queryMapper.selectById(query - 1000);
+            List<Double> queryVectorList = VectorUtil.strToDouble(queryVector.getVector(), 1);
+
+
+            for (GrandTruthResult grandTruthResult : result) {
+                pathList.add(grandTruthResult.getShot());
+
+                //特征向量，并转成浮点数组
+                List<Double> vectorDoubleList = VectorUtil.strToDouble(grandTruthResult.getVector(), 1);
+
+                //计算查询文本和图片的相似度得分
+                Double cosineSimilarity = VectorUtil.getCosineSimilarity(queryVectorList, vectorDoubleList);
+
+                //建立（路径，得分）的键值对
+                scoreMap.put(PathUtils.handleToGTPath(grandTruthResult.getShot()), cosineSimilarity);
+
+                //建立（路径，得分）的键值对
+                pathMap.put(PathUtils.handleToGTPath(grandTruthResult.getShot()), vectorDoubleList);
+            }
+
+            String str = getLabelsByQuery(queryVector.getQuery());
+            List<String> labels = new ArrayList<>(Arrays.asList(str.substring(1, str.length()-1).split("'")));
+            labels.removeIf(s -> s.length() < 2);
+            System.out.println(labels);
+
+            for (String label : labels) {
+                List<Double> labelVectorList = getTextVector(label);
+
+                for (String key:scoreMap.keySet()){
+                    List<Double> vectorDoubleList = pathMap.get(key);
+
+                    //计算查询文本和图片的相似度得分
+                    Double cosineSimilarity = VectorUtil.getCosineSimilarity(labelVectorList, vectorDoubleList);
+
+                    Double oldValue = scoreMap.get(key);
+
+                    //建立（路径，得分）的键值对
+                    scoreMap.replace(key, oldValue + cosineSimilarity);
+                }
+            }
+
+
+            //将（路径，得分）的键值对按得分降序
+            Map<String, Double> sortMap = VectorUtil.sortMapByValues(scoreMap);
+
+
+            //将路径存入urlList
+            savePathToUrlList(urlList, sortMap);
+//            System.out.println(urlList);
+
+
+            List<String> topList = pathList.subList(0, topK);
+
+            topList.replaceAll(PathUtils::handleToGTPath);
+
+            Collections.sort(topList);
+
+            List<GrandTruth> grandTruths = grandTruthMapper.selectList(null);
+            List<GrandTruth> queryGrandTruths = new ArrayList<>();
+
+            for (GrandTruth grandTruth : grandTruths) {
+                if (grandTruth.getQuery() == query) {
+                    queryGrandTruths.add(grandTruth);
+                } else if (grandTruth.getQuery() > query) {
+                    break;
+                }
+            }
+
+            int number = queryGrandTruths.size();
+            System.out.println("query " + query + " total true count = " + number);
+
+            int count = 0;
+
+            for (GrandTruth queryGrandTruth : queryGrandTruths) {
+                for (String s : topList) {
+                    if (Objects.equals(queryGrandTruth.getShot(), s)) {
+                        count++;
+                        //System.out.println(s);
+                    } else if (queryGrandTruth.getShot().compareTo(s) < 0) {
+                        break;
+                    }
+                }
+            }
+            total += count;
+
+            System.out.println("top K = " + topK);
+            System.out.println("predict true count = " + count);
+            System.out.println("precision@" + topK + " = " + ((double) count / topK));
+            System.out.println();
+        }
+
+        System.out.println("mean precision@" + topK + " = " + ((double) total) / topK / 20);
+    }
+
+
+    @Test
+    String getLabelsByQuery(String query){
+        StringBuilder strLabels = new StringBuilder();
+        //调用 python 函数
+        try {
+            //执行 py 文件
+            String[] args1 = new String[] { "E:\\Git\\lavis2\\venv\\Scripts\\python.exe", "E:\\Git\\lavis2\\keyphraseExtract.py", query };
+            Process proc = Runtime.getRuntime().exec(args1);
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+            //读取输出
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                strLabels.append(line);
+            }
+            in.close();
+
+            proc.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return strLabels.toString();
+    }
+
+    public List<Double> getTextVector(String query) {
+        List<Double> queryVector;
+
+        //调用 python 函数得到查询文本的特征向量
+        String[] args1 = new String[] { "E:\\Git\\lavis2\\venv\\Scripts\\python.exe", "E:\\Git\\lavis2\\textExtractor.py", query };
+        String strQueryVector = runPython(args1);
+
+        //将特征向量转化为浮点数组
+        queryVector = VectorUtil.queryStrToDouble(String.valueOf(strQueryVector));
+
+        return queryVector;
+    }
+
+
+    public String runPython(String[] args) {
+        StringBuilder strVector = new StringBuilder();
+        //调用 python 函数
+        try {
+            //执行 py 文件
+            Process proc = Runtime.getRuntime().exec(args);
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+            //读取特征向量
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                strVector.append(line);
+            }
+            in.close();
+
+            proc.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return strVector.toString();
+    }
 }

760251ad94d6653d33e65a22fb569a3e986eb4ff
Lunr_Lee <Lunr_Lee@outlook.com>
2022-12-05 18:51:45 +0800

    qmr test

--- src/test/java/whu/vbs/grandTruthTests.java
+++ src/test/java/whu/vbs/grandTruthTests.java
@@ -27,9 +27,13 @@ public class grandTruthTests {
     @Autowired
     QueryMapper queryMapper;
 
+    Map<String, Double> scoreMap = new HashMap<>();//（路径，得分）键值对
+    Map<String, List<Double>> vectorMap = new HashMap<>();//（路径，向量）键值对
+    List<String> pathList = new ArrayList<>();
+
 
     @Test
-    void test1() {
+    void initialSortingTest() {
 
 
         int topK = 200;
@@ -37,24 +41,23 @@ public class grandTruthTests {
 
 
         int query;
+        int count;
 
-        for (query = 1661; query <= 1680; query++) {
+        for (query = 1668; query <= 1668; query++) {
 
             String queryNumber = Integer.toString(query).substring(1);
             CsvReader reader = CsvUtil.getReader();
             String csvPath = "D:\\Download\\VBSDataset\\grand_truth\\" + queryNumber + ".csv";
             List<GrandTruthResult> result = reader.read(ResourceUtil.getUtf8Reader(csvPath), GrandTruthResult.class);
 
-            Map<String, Double> scoreMap = new HashMap<>();//（路径，得分）键值对
-            List<String> pathList = new ArrayList<>();
-            List<String> urlList = new ArrayList<>();//查询结果的路径
+
 
             Query queryVector = queryMapper.selectById(query - 1000);
             List<Double> queryVectorList = VectorUtil.strToDouble(queryVector.getVector(), 1);
 
 
             for (GrandTruthResult grandTruthResult : result) {
-                pathList.add(grandTruthResult.getShot());
+                pathList.add(PathUtils.handleToGTPath(grandTruthResult.getShot()));
 
                 //特征向量，并转成浮点数组
                 List<Double> vectorDoubleList = VectorUtil.strToDouble(grandTruthResult.getVector(), 1);
@@ -64,57 +67,95 @@ public class grandTruthTests {
 
                 //建立（路径，得分）的键值对
                 scoreMap.put(PathUtils.handleToGTPath(grandTruthResult.getShot()), cosineSimilarity);
+
+                //建立（路径，概率）的键值对
+                vectorMap.put(PathUtils.handleToGTPath(grandTruthResult.getShot()), vectorDoubleList);
             }
+
+            //initial
+            VectorUtil.mapNormalization(scoreMap);
+
             //将（路径，得分）的键值对按得分降序
             Map<String, Double> sortMap = VectorUtil.sortMapByValues(scoreMap);
 
-
+            List<String> urlList = new ArrayList<>();//查询结果的路径
             //将路径存入urlList
             savePathToUrlList(urlList, sortMap);
-//            System.out.println(urlList);
-
-
             List<String> topList = urlList.subList(0, topK);
 
-            Collections.sort(topList);
-
-            List<GrandTruth> grandTruths = grandTruthMapper.selectList(null);
-            List<GrandTruth> queryGrandTruths = new ArrayList<>();
-
-            for (GrandTruth grandTruth : grandTruths) {
-                if (grandTruth.getQuery() == query) {
-                    queryGrandTruths.add(grandTruth);
-                } else if (grandTruth.getQuery() > query) {
-                    break;
-                }
-            }
-
-            int number = queryGrandTruths.size();
-            System.out.println("query " + query + " total true count = " + number);
-
-            int count = 0;
-
-            for (GrandTruth queryGrandTruth : queryGrandTruths) {
-                for (String s : topList) {
-                    if (Objects.equals(queryGrandTruth.getShot(), s)) {
-                        count++;
-                        //System.out.println(s);
-                    } else if (queryGrandTruth.getShot().compareTo(s) < 0) {
-                        break;
-                    }
-                }
-            }
+            count = getGTMatch(topList, query);
             total += count;
 
             System.out.println("top K = " + topK);
             System.out.println("predict true count = " + count);
             System.out.println("precision@" + topK + " = " + ((double) count / topK));
             System.out.println();
+
+
+            String[] shots = new String[]{"shot00152_64", "shot01513_90", "shot01898_59", "shot02497_123", "shot04871_23"};
+
+            for (String shot : shots) {
+                //qmr
+                qmr(shot);
+                VectorUtil.mapNormalization(scoreMap);
+                //将（路径，得分）的键值对按得分降序
+                Map<String, Double> reRankSortMap = VectorUtil.sortMapByValues(scoreMap);
+
+
+                //将路径存入urlList
+                List<String> reRankUrlList = new ArrayList<>();//查询结果的路径
+                savePathToUrlList(reRankUrlList, reRankSortMap);
+                List<String> reRandTopList = reRankUrlList.subList(0, topK);
+
+                count = getGTMatch(reRandTopList, query);
+                total += count;
+
+                System.out.println("top K = " + topK);
+                System.out.println("predict true count = " + count);
+                System.out.println("precision@" + topK + " = " + ((double) count / topK));
+                System.out.println();
+            }
+
+
         }
 
         System.out.println("mean precision@" + topK + " = " + ((double) total) / topK / 20);
 
 
+    }
+
+    int getGTMatch(List<String> topList, int query) {
+        Collections.sort(topList);
+        System.out.println(topList);
+
+        List<GrandTruth> grandTruths = grandTruthMapper.selectList(null);
+        List<GrandTruth> queryGrandTruths = new ArrayList<>();
+
+        for (GrandTruth grandTruth : grandTruths) {
+            if (grandTruth.getQuery() == query) {
+                queryGrandTruths.add(grandTruth);
+            } else if (grandTruth.getQuery() > query) {
+                break;
+            }
+        }
+
+        int number = queryGrandTruths.size();
+        System.out.println("query " + query + " total true count = " + number);
+
+        int count = 0;
+
+        for (GrandTruth queryGrandTruth : queryGrandTruths) {
+            for (String s : topList) {
+                if (Objects.equals(queryGrandTruth.getShot(), s)) {
+                    count++;
+                    //System.out.println(s);
+                } else if (queryGrandTruth.getShot().compareTo(s) < 0) {
+                    break;
+                }
+            }
+        }
+
+        return count;
     }
 
 
@@ -197,7 +238,7 @@ public class grandTruthTests {
     }
 
     @Test
-    void test8() {
+    void feedbackTest() {
         int query = 1680;
         int[] topKs = {5, 10, 100, 200};
 
@@ -455,7 +496,7 @@ public class grandTruthTests {
             Process proc = Runtime.getRuntime().exec(args);
             BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 
-            //读取特征向量
+            //读取输出
             String line = null;
             while ((line = in.readLine()) != null) {
                 strVector.append(line);
@@ -468,4 +509,23 @@ public class grandTruthTests {
         }
         return strVector.toString();
     }
+
+    void qmr(String shot) {
+        double alpha_dt = Math.sqrt(scoreMap.get(shot));
+        double beta_dt = Math.sqrt(1 - scoreMap.get(shot));
+
+        for (String path : pathList) {
+            double probability = scoreMap.get(path);
+
+            double cosineSimilarity = VectorUtil.getCosineSimilarity(vectorMap.get(path), vectorMap.get(shot));
+
+            double alpha_d = Math.sqrt(probability);
+            double beta_d = Math.sqrt(1 - probability);
+            double my_lambda = alpha_d * alpha_dt + beta_d * beta_dt;
+            double my_probability = Math.pow(my_lambda * alpha_dt, 2);
+            scoreMap.replace(path, my_probability * cosineSimilarity);
+        }
+    }
+
+
 }

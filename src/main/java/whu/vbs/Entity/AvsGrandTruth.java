d1f953cc898926187d69f7b90712dd5bd79d7374
Lunr_Lee <Lunr_Lee@outlook.com>
2022-12-10 22:57:56 +0800

    AvsGrandTruth

---
+++ src/main/java/whu/vbs/Entity/AvsGrandTruth.java
@@ -0,0 +1,11 @@
+package whu.vbs.Entity;
+
+import lombok.Data;
+
+@Data
+public class AvsGrandTruth {
+    Integer queryId;
+    String videoId;
+    String startTime;
+    String endTime;
+}
---
+++ src/main/java/whu/vbs/Mapper/AvsGrandTruthMapper.java
@@ -0,0 +1,9 @@
+package whu.vbs.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+import whu.vbs.Entity.AvsGrandTruth;
+
+@Repository
+public interface AvsGrandTruthMapper extends BaseMapper<AvsGrandTruth> {
+}

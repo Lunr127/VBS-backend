f66b0b52210bcfb00e2d05ae5b304f07e41e7c35
Lunr_Lee <Lunr_Lee@outlook.com>
2023-01-02 23:03:48 +0800

    add KisvQuery

---
+++ src/main/java/whu/vbs/Entity/CsvFile/KisvQuery.java
@@ -0,0 +1,9 @@
+package whu.vbs.Entity.CsvFile;
+
+import lombok.Data;
+
+@Data
+public class KisvQuery {
+    Integer queryId;
+    String vector;
+}
---
+++ src/main/java/whu/vbs/Mapper/KisvQueryMapper.java
@@ -0,0 +1,9 @@
+package whu.vbs.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+import whu.vbs.Entity.CsvFile.KisvQuery;
+
+@Repository
+public interface KisvQueryMapper extends BaseMapper<KisvQuery> {
+}

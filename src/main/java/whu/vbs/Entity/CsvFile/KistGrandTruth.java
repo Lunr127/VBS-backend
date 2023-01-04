ab033cefd9d25fe05908b98d8c68c6db400e802d
Lunr_Lee <Lunr_Lee@outlook.com>
2023-01-04 10:24:39 +0800

    add KistGrandTruth

---
+++ src/main/java/whu/vbs/Entity/CsvFile/KistGrandTruth.java
@@ -0,0 +1,11 @@
+package whu.vbs.Entity.CsvFile;
+
+import lombok.Data;
+
+@Data
+public class KistGrandTruth {
+    Integer queryId;
+    String videoId;
+    String startTime;
+    String endTime;
+}

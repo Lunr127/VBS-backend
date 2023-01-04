9f612dc3d1e4373234540ed5733a40f496ec4431
Lunr_Lee <Lunr_Lee@outlook.com>
2023-01-04 10:24:53 +0800

    add KistQuery

---
+++ src/main/java/whu/vbs/Entity/CsvFile/KistQuery.java
@@ -0,0 +1,9 @@
+package whu.vbs.Entity.CsvFile;
+
+import lombok.Data;
+
+@Data
+public class KistQuery {
+    Integer queryId;
+    String vector;
+}
---
+++ src/main/java/whu/vbs/Mapper/KistQueryMapper.java
@@ -0,0 +1,9 @@
+package whu.vbs.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+import whu.vbs.Entity.CsvFile.KistQuery;
+
+@Repository
+public interface KistQueryMapper extends BaseMapper<KistQuery> {
+}

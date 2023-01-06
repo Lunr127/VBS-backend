28b9397d390fcfbcd4afd84ad0bd37fdbd738161
Lunr_Lee <Lunr_Lee@outlook.com>
2023-01-06 16:36:33 +0800

    add some method in PathUtils

--- src/main/java/whu/vbs/utils/PathUtils.java
+++ src/main/java/whu/vbs/utils/PathUtils.java
@@ -1,7 +1,10 @@
 package whu.vbs.utils;
 
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.*;
 
 public class PathUtils {
     public static String handlePath(String prePath) {
@@ -35,5 +38,56 @@ public class PathUtils {
         }
 
         return pathList;
+    }
+
+    public static String marinePathIdToFrameId(String pathId){
+        int index = pathId.indexOf(".");
+        String frameId = pathId.substring(0, index);
+        frameId = frameId.replace("/", "_");
+        return frameId;
+    }
+
+
+    public static void savePathToUrlList(List<String> urlList, Map<String, Double> sortMap) {
+        for (String path : sortMap.keySet()) {
+            urlList.add(path);
+        }
+    }
+
+
+    public static List<Map<String, String>> urlToBase64List(List<String> urlList, int showTopK){
+        List<Map<String, String>> base64List = new ArrayList<>();
+        for (String shot : urlList.subList(0, showTopK)) {
+            Map<String, String> base64Map = new HashMap<>();// (base64，路径)键值对
+            String base64 = "data:image/png;base64," + imgToBase64(shot);
+            base64Map.put("shot", shot);
+            base64Map.put("base64", base64);
+            base64List.add(base64Map);
+        }
+        return base64List;
+    }
+
+    public static String imgToBase64(String shot) {
+        String path = "";
+        if (Integer.parseInt(shot.substring(4, 9)) <= 7475) {
+            path = "F:\\VBSDataset\\V3C1\\thumbnails\\" + shot.substring(4, 9) + "\\" + shot + ".png";
+        } else {
+            path = "F:\\VBSDataset\\V3C2\\thumbnails\\" + shot.substring(4, 9) + "\\" + shot + ".png";
+        }
+
+        File file = new File(path);
+
+        byte[] data = null;
+        try {
+            InputStream in = Files.newInputStream(file.toPath());
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Base64.Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(data);
     }
 }

47babcf4a9a8023567cd7e8188cd0fe571f3d661
Lunr_Lee <Lunr_Lee@outlook.com>
2023-03-09 19:47:30 +0800

    add ImageFilter to upload image to interface

---
+++ src/main/java/whu/vbs/Entity/CsvFile/ImageFilter.java
@@ -0,0 +1,8 @@
+package whu.vbs.Entity.CsvFile;
+
+import lombok.Data;
+
+@Data
+public class ImageFilter {
+    String shot;
+}
---
+++ src/main/java/whu/vbs/Mapper/ImageFilterMapper.java
@@ -0,0 +1,9 @@
+package whu.vbs.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+import whu.vbs.Entity.CsvFile.ImageFilter;
+
+@Repository
+public interface ImageFilterMapper extends BaseMapper<ImageFilter> {
+}
